eef9a14389ec778e8643ea6070c907fd963ae089
Carl-Fredrik Enell <fredrik@kyla.kiruna.se>
2019-01-19 19:37:21 +0100

    WIP commit

---
+++ wsprfile.py
@@ -0,0 +1,33 @@
+# A first try at WSPR transmission generation.
+
+import encode
+import numpy as np
+
+### Message details
+callsign = 'SM2YHP'
+locator = 'KP07'
+dbm = 30
+base_freq = 7038600
+
+
+### WSPR details
+msg_len= 162
+sample_rate = 30e6
+fsk_shift = 1.4648
+baud_len = int(8192.0/12000.0*sample_rate) # samples
+
+### Encode message
+fsks = encode.wspr_encode(callsign, locator, dbm)
+assert len(fsks) == msg_len
+
+### Write raw sample file
+sample_file = f'{callsign}_{locator}_{dbm}_{base_freq}_{sample_rate}.dat'
+sample_index = 0
+with open(sample_file,'bw') as sf:
+    for baud in fsks:
+        freq = base_freq + baud*fsk_shift;
+        phases = 2*np.pi*freq*(sample_index + np.arange(baud_len))/sample_rate
+        samples = 128.0 + 128.0 * np.cos(phases)
+        samples.astype(np.uint8).tofile(sf)        
+        sample_index=sample_index + baud_len
+        print(f'Written {baud}')
