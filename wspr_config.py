61f6c53aea92049489f877feed687a77a42cf63f
Carl-Fredrik Enell <fredrik@kyla.kiruna.se>
2019-01-20 18:19:50 +0100

    Changed to numpy memmap file to save waveform. Avoiding repeated waveform calcúlation if sample file exists.

--- wspr_config.py
+++ wspr_config.py
@@ -1,8 +1,10 @@
 ### Configuration for wsprtx
 
 ### fl2k_tcp config
+tcp_client = '/usr/local/bin/fl2k_tcp'
 tcp_host = '127.0.0.1' # Bind address
 tcp_port = 1234 #TCP port
+fl2k_dev = 0
 
 ### Message defaults
 callsign = 'SK2GJ'
--- wsprtcp.py
+++ wsprtcp.py
@@ -1,16 +1,16 @@
 ### WSPR transmission over fl2k_tcp
 ### C-F Enell 20190120
 
-# Default libraries
+### Default libraries
 import numpy as np
-import mmap
-import time
 import socket
+import subprocess
+import time
+from os.path import isfile
 
-# Modules
-import wspr_config
+### Modules
 import encode
-
+import wspr_config
 
 ### Waveform writer
 def sample_producer(msg, base_freq, sample_rate):
@@ -22,54 +22,72 @@ def sample_producer(msg, base_freq, sample_rate):
 
     assert len(msg) == msg_len
 
-    wave = mmap.mmap(-1, msg_len * baud_len)
+    ## Open waveform file if exists
+    filename=f'/tmp/wspr_{base_freq}_{sample_rate}.dat'
+
+    if isfile(filename):
+        return(np.memmap(filename, mode='r', dtype='uint8'), baud_len)
+
+    ## Else create waveform file
+    wave = np.memmap(filename, mode='w+', dtype='uint8', shape = (1,baud_len*sample_rate))
     baud_no=1
     sample0=0
     si=np.arange(baud_len)
-    
+
+    ## Fill waveform with 4FSK samples 
     for baud in msg:
         freq = base_freq + baud*fsk_shift;
         phases = 2*np.pi*freq*(sample0 + si)/sample_rate
+        ## 8-bit samples
         wave[sample0:sample0+baud_len] = ( 128.0 + 128.0 * np.cos(phases) ).astype(np.uint8)
         sample0=sample0 + baud_len
-        print(f'sample_producer: Done baud {baud_no} FSK shift {baud}')
+        print(f'sample_producer: Done baud {baud_no}:{msg_len} 4FSK shift {baud}')
         baud_no=baud_no + 1
         
     return(wave, baud_len)
     
 if __name__ == '__main__':
 
-    ### Set up TCP server
+    ### Prepare WSPR message Tx
+    
+    ## Encode message
+    print(f'wsprtcp: Encoding message {wspr_config.callsign} {wspr_config.locator} {wspr_config.dbm}')
+    msg = encode.wspr_encode(wspr_config.callsign, wspr_config.locator, wspr_config.dbm)
+
+    ## Pre-create waveform
+    print(f'wsprtcp: Creating waveform f={wspr_config.base_freq} Hz at {wspr_config.sample_rate} Hz. This will take time.')
+    wave, baud_len=sample_producer(msg, wspr_config.base_freq, wspr_config.sample_rate) #mmap
+
+    ## Set up TCP server
     print(f'wsprtcp: Creating sample server on {wspr_config.tcp_host}:{wspr_config.tcp_port}')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((wspr_config.tcp_host, wspr_config.tcp_port))
     s.listen(1)
+
+    ## Spawn the fl2k client
+    client_args=[wspr_config.tcp_client,'-a',wspr_config.tcp_host,'-p',f'{wspr_config.tcp_port}','-d',f'{wspr_config.fl2k_dev}','-s',f'{wspr_config.sample_rate}']
+    print('wsprtcp: Spawning ' + client_args)
+    tcp_client=subprocess.Popen(client_args)
+
+    ## Connect client
     conn, addr = s.accept()
-
-    ### Encode message
-    print(f'wsprtcp: Encoding message {wspr_config.callsign} {wspr_config.locator} {wspr_config.dbm}')
-    msg = encode.wspr_encode(wspr_config.callsign, wspr_config.locator, wspr_config.dbm)
-
-    ### Pre-create waveform
-    print(f'wsprtcp: Creating waveform f={wspr_config.base_freq} Hz at {wspr_config.sample_rate} Hz. This will take time.')
-    wave, baud_len=sample_producer(msg, wspr_config.base_freq, wspr_config.sample_rate) #mmap
-
+        
     ### Main loop
     for loop_no in range(wspr_config.nloops):
 
         ## Wait for time to start Tx  
         print(f'wsprtcp: Waiting to start cycle {loop_no+1} of {wspr_config.nloops}')
-
         while(True):
             if time.time() % 120 < 0.05:
                 break
             time.sleep(0.01)
 
+        ## Transmit one sequence
         print(f'wsprtcp: Transmitting message')
-
         bufstart=0
         stop = False
         while True:
+            ## Send one FSK baud to FL2K
             bufend = bufstart + baud_len
             if bufend > len(wave):
                 bufend = len(wave)
@@ -80,11 +98,13 @@ if __name__ == '__main__':
             if stop:
                 break
             bufstart=bufend
-            
-        print(f'wsprtcp: Done cycle {loop_no+1} of {nloops}')
-        
-    ### Done
-    wave.close()
-    conn.close()
-    print('wsprtcp: Done')
+        ## Done transmit sequence
+        print(f'wsprtcp: Done cycle {loop_no+1} of {wspr_config.nloops}')
 
+    ###  Done main loop
+    wave.flush() # Make sure to save on disk
+    del(wave)    # Close mmapped file
+    conn.close()
+    tcp_client.terminate()
+    print('wsprtcp: Done')
+### End of wsprtcp.py
